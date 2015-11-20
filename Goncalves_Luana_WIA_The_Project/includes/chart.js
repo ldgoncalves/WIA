bd652e53b6f712faabdea9e7c6ae0d9c1979c0ec
luluphotog <ldgoncalves@fullsail.edu>
2015-11-19 23:39:07 -0500

    edits to chart

--- Goncalves_Luana_WIA_The_Project/includes/chart.js
+++ Goncalves_Luana_WIA_The_Project/includes/chart.js
@@ -2,6 +2,24 @@
  * Created by luanagoncalves on 11/19/15.
  */
 
+$(function(){
+
+    /* Dropdown effect */
+
+    $('ul.parent > li').hover(function(){
+        $(this).find('ul.child').show(200);
+    }, function(){
+        $(this).find('ul.child').hide(200);
+    });
+
+    $('ul.child > li').hover(function(){
+        $(this).find('ul.sibling').show(200);
+    }, function(){
+        $(this).find('ul.sibling').hide(200);
+    });
+
+});
+
 google.load("visualization", "1", {packages:["corechart"]});
 
 google.setOnLoadCallback(drawChart);
